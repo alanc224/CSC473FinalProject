59dc804e1299d71f878373b198df30919d07fab8
DTang127 <93285387+DTang127@users.noreply.github.com>
2024-04-03 13:19:16 -0400

    changes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, render_template, url_for, request
+from flask import Flask, redirect, render_template, url_for, request, jsonify
 from flask_login import login_user, LoginManager, login_required, logout_user, current_user,UserMixin
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length,ValidationError
 from flask_bcrypt import Bcrypt
+from models import db, User
 
 
 app = Flask(__name__)
@@ -12,22 +12,19 @@ bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SECRETKEY'
-db = SQLAlchemy(app)
+db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+with app.app_context():
+    db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -72,16 +69,32 @@ def logout():
 
 @app.route('/register',methods=['GET','POST'])
 def register():
-    form = RegisterForm()
+    username = request.json['username']
+    password = request.json['password']
 
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
+    user_exists = User.query.filter_by(username=username).first() is not None
+    if user_exists:
+        return jsonify({"error": "Username is already taken."}), 409
+        
+    hashed_password = bcrypt.generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
 
-    return render_template('register.html',form=form)
+    return jsonify({
+        'id': new_user.id,
+        'username': new_user.username
+    })
+    # form = RegisterForm()
+
+    # if form.validate_on_submit():
+    #     hashed_password = bcrypt.generate_password_hash(form.password.data)
+    #     new_user = User(username=form.username.data, password=hashed_password)
+    #     db.session.add(new_user)
+    #     db.session.commit()
+    #     return redirect(url_for('login'))
+
+    # return render_template('register.html',form=form)
 
 
 if __name__ == '__main__':
