a5196bdfed1b8001d45c195d0335563e6a0a3a28
DTang127 <93285387+DTang127@users.noreply.github.com>
2024-04-06 18:25:47 -0400

    added pages

---
+++ pages/homepage.js
@@ -0,0 +1,19 @@
+import React, { } from "react";
+ 
+import {Link} from 'react-router-dom';
+ 
+export default function HomePage(){
+ 
+  return (
+    <div>
+        <div className="container h-100">
+            <div className="row h-100">
+                <div className="col-12">
+                    <h1>Welcome to this React Application</h1>
+                    <p><Link to="/login" className="btn btn-success">Login</Link> | <Link to="/register" className="btn btn-success">register</Link> </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+}
---
+++ pages/registerpage.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import {useNavigate} from "react-router-dom";
+ 
+export default function RegisterPage(){
+ 
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+   
+    const navigate = useNavigate();
+     
+    const registerUser = () => {
+        axios.post('http://127.0.0.1:5000/signup', {
+            username: username,
+            password: password
+        })
+        .then(function (response) {
+             console.log(response);
+            navigate("/");
+        })
+        .catch(function (error) {
+            console.log(error, 'error');
+            if (error.response.status === 401) {
+                alert("Invalid credentials");
+            }
+        });
+    };
+     
+    let imgs = [
+      'https://as2.ftcdn.net/v2/jpg/03/39/70/91/1000_F_339709132_H9HSSTtTmayePcbARkTSB2qoZTubJ6bR.jpg',
+    ];
+     
+  return (
+    <div>
+        <div className="container h-100">
+          <div className="container-fluid h-custom">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form>
+                  <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                    <p className="lead fw-normal mb-0 me-3">Create Your Account</p>
+                  </div>
+ 
+                  <div className="form-outline mb-4">
+                    <input type="username" value={username} onChange={(e) => setUsername(e.target.value)} id="form3Example3" className="form-control form-control-lg" placeholder="Enter a valid username address" />
+                    <label className="form-label" for="form3Example3">username address</label>
+                  </div>
+ 
+             
+                  <div className="form-outline mb-3">
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="form3Example4" className="form-control form-control-lg" placeholder="Enter password" />
+                    <label className="form-label" for="form3Example4">Password</label>
+                  </div>
+ 
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="form-check mb-0">
+                      <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
+                      <label className="form-check-label" for="form2Example3">
+                        Remember me
+                      </label>
+                    </div>
+                    <a href="#!" className="text-body">Forgot password?</a>
+                  </div>
+ 
+                  <div className="text-center text-lg-start mt-4 pt-2">
+                    <button type="button" className="btn btn-primary btn-lg" onClick={() => registerUser()} >Sign Up</button>
+                    <p className="small fw-bold mt-2 pt-1 mb-0">Login to your account <a href="/login" className="link-danger">Login</a></p>
+                  </div>
+ 
+                </form>
+              </div>
+              <div className="col-md-9 col-lg-6 col-xl-5">
+                <img src={imgs[0]} className="img-fluid"/>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
