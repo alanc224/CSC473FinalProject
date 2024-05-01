821a5064d1a0762f9283c9c6ae5a756e04022e10
alanc224 <alancgc22@gmail.com>
2024-05-01 19:16:50 -0400

    react stuff working

---
+++ myreactapp/src/App.js
@@ -0,0 +1,26 @@
+import React, { } from 'react';
+import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomePage from './pages/homepage';
+import LoginPage from './pages/loginpage';
+import RegisterPage from './pages/registerpage';
+
+function App() {
+  return (
+    <div className='vh-100 gradient-custom'>
+      <div className='container'>
+        <h1 className='page-header text-center'>React and Python Flask Login Register</h1>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />    
+        </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
