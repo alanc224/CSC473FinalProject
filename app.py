ce297379a55ef30f6dd4a5a30d63bb005ae7b56c
alanc224 <alancgc22@gmail.com>
2024-05-01 14:52:17 -0400

    Updates

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, url_for, request, jsonify, session
 from flask_login import login_user, LoginManager, login_required, logout_user, current_user,UserMixin
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from flask_cors import CORS, cross_origin
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length,ValidationError
 from flask_bcrypt import Bcrypt
@@ -17,44 +17,58 @@ load_dotenv()
 stripe.api_key = os.getenv('STRIPE_API_SKEY')
 
 bcrypt = Bcrypt(app)
-CORS(app, supports_credentials=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SECRETKEY'
-db.init_app(app)
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
-with app.app_context():
-    db.create_all()
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], 
+                           render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], 
+                             render_kw={"placeholder": "Password"})
+
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
         if existing_user_username:
-            raise ValidationError('That username is not valid. Please choose a different username.')
+            raise ValidationError(
+                'Please choose a different username.')
+
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], 
+                           render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], 
+                             render_kw={"placeholder": "Password"})
+
     submit = SubmitField('Login')
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login',methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -65,10 +79,12 @@ def login():
                 return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
+
 @app.route('/payment_page', methods=['GET', 'POST'])
 @login_required
 def dashboard():
     return render_template('payment_page.html')
+
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -76,7 +92,8 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/register',methods=['GET','POST'])
+
+@ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
 
@@ -87,8 +104,8 @@ def register():
         db.session.commit()
         return redirect(url_for('login'))
 
-    return render_template('register.html',form=form)
+    return render_template('register.html', form=form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
