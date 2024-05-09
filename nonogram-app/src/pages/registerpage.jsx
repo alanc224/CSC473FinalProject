3ae12ea88d191b2e0664613e1bb5ab51680c661c
Zuhayer Alvi <azuhayer@gmail.com>
2024-05-08 22:08:59 -0400

    register page UI

--- nonogram-app/src/pages/registerpage.jsx
+++ nonogram-app/src/pages/registerpage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
+import Navbar from "./NavBar";
+import './registerpage.css';
  
 export default function RegisterPage(){
  
@@ -26,54 +28,35 @@ export default function RegisterPage(){
         });
     };
      
-    let imgs = [
-      'https://as2.ftcdn.net/v2/jpg/03/39/70/91/1000_F_339709132_H9HSSTtTmayePcbARkTSB2qoZTubJ6bR.jpg',
-    ];
-     
   return (
-    <div>
-        <div className="container h-100">
-          <div className="container-fluid h-custom">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                <form>
-                  <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
-                    <p className="lead fw-normal mb-0 me-3">Create Your Account</p>
-                  </div>
- 
-                  <div className="form-outline mb-4">
-                    <input type="username" value={username} onChange={(e) => setUsername(e.target.value)} id="form3Example3" className="form-control form-control-lg" placeholder="Enter a valid username address" />
-                    <label className="form-label" for="form3Example3">username address</label>
-                  </div>
- 
-             
-                  <div className="form-outline mb-3">
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="form3Example4" className="form-control form-control-lg" placeholder="Enter password" />
-                    <label className="form-label" for="form3Example4">Password</label>
-                  </div>
- 
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="form-check mb-0">
-                      <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                      <label className="form-check-label" for="form2Example3">
-                        Remember me
-                      </label>
-                    </div>
-                    <a href="#!" className="text-body">Forgot password?</a>
-                  </div>
- 
-                  <div className="text-center text-lg-start mt-4 pt-2">
-                    <button type="button" className="btn btn-primary btn-lg" onClick={() => registerUser()} >Sign Up</button>
-                    <p className="small fw-bold mt-2 pt-1 mb-0">Login to your account <a href="/login" className="link-danger">Login</a></p>
-                  </div>
- 
-                </form>
-              </div>
-              <div className="col-md-9 col-lg-6 col-xl-5">
-                <img src={imgs[0]} className="img-fluid"/>
-              </div>
+    <div className="registerpage">
+      <Navbar />
+        <div className="register-form-container">
+          <form>
+            <div>
+              <p>Create Your Account</p>
             </div>
-          </div>
+
+            <div>
+              <input type="username" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter a valid email address" />
+              <label>Email Address</label>
+            </div>
+
+            <div>
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+              <label>Password</label>
+            </div>
+
+            <div>
+              <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm password" />
+              <label>Confirm Password</label>
+            </div>
+
+            <div>
+              <button type="button" onClick={registerUser}>Register</button>
+              <p>Already have an account? <a href="/login">Login</a></p>
+            </div>
+          </form>
         </div>
     </div>
   );
